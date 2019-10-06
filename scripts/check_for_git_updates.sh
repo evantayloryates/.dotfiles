f8be45b59623a516b0e33c8d78dd2935a6f6af41
evantayloryates <evantayloryates@gmail.com>
2019-10-05 18:32:55 -0700

    updating detection script

--- scripts/check_for_git_updates.sh
+++ scripts/check_for_git_updates.sh
@@ -1,8 +1,29 @@
 # Check for new files
-git --git-dir=$DOTFILES_DIR/.git --work-tree=/$DOTFILES_DIR pull
+git_pull_msg=$(git --git-dir=$DOTFILES_DIR/.git --work-tree=/$DOTFILES_DIR pull 2>&1)
 
-# If merge conflict, throw system notifcation
-osascript -e 'display notification "Dofiles merge required!"'
+
+if [ "$git_pull_msg" == "Already up to date." ]; then
+    echo "No dotfile changes detected"
+else
+	echo "Dotfile changes detected"
+	if [[ $git_pull_msg == *"error: Your local changes to the following files would be overwritten by merge"* ]]; then
+		echo "Merge error anticipated"
+		
+		# If merge conflict, throw system notifcation
+		osascript -e 'display notification "dotfiles repo: merge required"'
+
+		# Exit script to avoid adding and commitings files
+		exit 0
+	else
+		echo "No merge conflicts... pulling changes..."
+	fi
+fi
+
+if [[ `git --git-dir=$DOTFILES_DIR/.git --work-tree=/$DOTFILES_DIR status --porcelain` ]]; then
+  echo "Local changes detected"
+else
+  echo "No local changes"
+fi
 
 # Else check if there are pending changes in dotfiles repo
 git --git-dir=$DOTFILES_DIR/.git --work-tree=/$DOTFILES_DIR status
