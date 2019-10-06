e32143519c25d086bc911ce05bdf9c50874c6381
evantayloryates <evantayloryates@gmail.com>
2019-10-05 18:56:38 -0700

    auto commit from script

--- scripts/check_for_git_updates.sh
+++ scripts/check_for_git_updates.sh
@@ -5,7 +5,7 @@ printf "${COLOR_WHITE}Remote Changes: "
 if [ "$git_pull_msg" == "Already up to date." ]; then
     printf "${COLOR_RED}NO${COLOR_WHITE}\n"
 else
-	printf "${COLOR_LIGHTGREEN}YES${COLOR_WHITE}\n"
+	printf "${COLOR_GREEN}YES${COLOR_WHITE}\n"
 	printf "Merge Safe: "
 	if [[ $git_pull_msg == *"error: Your local changes to the following files would be overwritten by merge"* ]]; then
 		printf "${COLOR_RED}NO${COLOR_WHITE}\n"
@@ -16,14 +16,14 @@ else
 		# Exit script to avoid adding and commitings files
 		exit 0
 	else
-		printf "${COLOR_LIGHTGREEN}YES${COLOR_WHITE}\n"
+		printf "${COLOR_GREEN}YES${COLOR_WHITE}\n"
 		echo "No merge conflicts... pulling changes..."
 	fi
 fi
 
 printf "Local Changes: "
 if [[ `git --git-dir=$DOTFILES_DIR/.git --work-tree=/$DOTFILES_DIR status --porcelain` ]]; then
-  printf "${COLOR_LIGHT_GREEN}YES${COLOR_WHITE}\n"
+  printf "${COLOR_GREEN}YES${COLOR_WHITE}\n"
   # Else check if there are pending changes in dotfiles repo
   echo "Adding all files..."
   git_add_msg=$(git --git-dir=$DOTFILES_DIR/.git --work-tree=/$DOTFILES_DIR add .)
