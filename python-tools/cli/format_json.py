d429672993425fdf0043948a95fadc31038f4802
evantayloryates <evantayloryates@gmail.com>
2020-02-05 13:16:14 -0800

    auto commit from script

--- python-tools/cli/format_json.py
+++ python-tools/cli/format_json.py
@@ -1,9 +1,16 @@
 import json
 import sys
-
+import os
 try:
-	file = argv[1]
+	file = sys.argv[1]
 except:
 	print("Please include a file to format")
+	exit()
 
-print(file)
+f = open(file, 'r')
+pretty_contents = json.loads(f.read())
+f.close()
+
+f = open(file, 'w')
+f.write(json.dumps(pretty_contents, indent=2))
+f.close()
