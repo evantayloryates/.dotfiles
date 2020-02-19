f17bc8d23f392402e5391c5a355a63e3efb3ebf1
evantayloryates <evantayloryates@gmail.com>
2020-02-19 14:48:29 -0800

    auto commit from script

--- python-tools/tools/utils.py
+++ python-tools/tools/utils.py
@@ -4,6 +4,7 @@ import sys
 import psutil
 import datetime as dt
 from inspect import isclass
+from pympler.asizeof import asizeof
 
 # ARRAY PRINT
 def ap(a, ll=1000, ind=True, indent_cnt=0):
@@ -135,29 +136,11 @@ def mem():
 
 # BYTE SIZE
 def bsize(obj):
-    raw_bytes = get_size(obj)
-    pbytes(get_size(raw_bytes))
+    if type(obj) == list:
 
-# HELPER FOR bsize
-def get_size(obj, seen=None):
-    """Recursively finds size of objects"""
-    size = sys.getsizeof(obj)
-    if seen is None:
-        seen = set()
-    obj_id = id(obj)
-    if obj_id in seen:
-        return 0
-    # Important mark as seen *before* entering recursion to gracefully handle
-    # self-referential objects
-    seen.add(obj_id)
-    if isinstance(obj, dict):
-        size += sum([get_size(v, seen) for v in obj.values()])
-        size += sum([get_size(k, seen) for k in obj.keys()])
-    elif hasattr(obj, '__dict__'):
-        size += get_size(obj.__dict__, seen)
-    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
-        size += sum([get_size(i, seen) for i in obj])
-    return size
+    raw_bytes = get_size(obj)
+    pbytes(asizeof(raw_bytes))
+
 
 def dline():
     CURSOR_UP_ONE = '\x1b[1A'
