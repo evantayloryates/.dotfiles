68222a4880dc37b249b9eec311c1b081a80e375e
evantayloryates <evantayloryates@gmail.com>
2020-02-18 19:29:48 -0800

    auto commit from script

--- scripts/check_for_git_updates.sh
+++ scripts/check_for_git_updates.sh
@@ -1,6 +1,6 @@
 # Check for new files
 git_pull_msg=$(git --git-dir=$DOTFILES_DIR/.git --work-tree=/$DOTFILES_DIR pull 2>&1)
-
+printf "${git_pull_msg}"
 printf "${COLOR_WHITE}Remote Changes: "
 if [ "$git_pull_msg" == "Already up to date." ]; then
     printf "${COLOR_RED}NO${COLOR_WHITE}\n"
