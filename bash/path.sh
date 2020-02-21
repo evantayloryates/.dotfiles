34aa4a8fa339a2e8feb57762e929eaf0eb044793
Evan Taylor Yates <evantayloryates>
2020-02-21 09:31:07 -0800

    auto commit from script

---
+++ bash/path.sh
@@ -0,0 +1,17 @@
+# TYPE: path
+# DESC: hub
+PATH=$PATH:/Users/etyates/bin
+
+# TYPE: path
+# DESC: npm global install directory
+PATH=$PATH:/Users/etyates/.npm-global/bin
+
+# TYPE: path
+# DESC: npm global install directory
+PATH=$PATH:/Users/etyates/node_modules/polymer-cli/bin/polymer.js
+
+# TYPE: path
+# DESC: adds 'subl' command to the active PATH
+PATH=$PATH:${EDITOR_APP}/Contents/SharedSupport/bin
+
+export PATH
