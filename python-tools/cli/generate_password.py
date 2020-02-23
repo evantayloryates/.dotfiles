24d3a81b22791105bfbf9cb0a9bd1b83f5f9cd22
evantayloryates <evantayloryates@gmail.com>
2020-02-23 14:48:39 -0800

    auto commit from script

--- python-tools/cli/generate_password.py
+++ python-tools/cli/generate_password.py
@@ -1,3 +1,24 @@
-import sys
+#!/usr/bin/python
 
-print(sys.argv)
+import sys
+import string
+import random
+
+from tools import *
+
+DEFAULT_LENGTH = 25
+AVAILABLE_CHARS = string.ascii_lowercase + string.digits
+
+
+def gen_password(l, chars=AVAILABLE_CHARS):
+	return ''.join(random.choice(chars) for _ in range(l))
+
+
+if __name__ == "__main__":
+	
+	try:
+		pw_length = sys.argv[1]
+	except:
+		pw_length = DEFAULT_LENGTH
+
+	gen_password(pw_length)
